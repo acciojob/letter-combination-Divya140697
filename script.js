2bfa2868e8457ca6e5125ce906e6d4e3d85b6575
Divya140697 <125757414+Divya140697@users.noreply.github.com>
2023-06-26 16:12:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+function letterCombinations(digits) {
+  const digitToLetters = {
+    '0': '0',
+    '1': '1',
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+  };
+
+  const combinations = [];
+
+  if (digits.length === 0) {
+    return combinations;
+  }
+
+  backtrack('', digits);
+
+  return combinations;
+
+  function backtrack(combination, nextDigits) {
+    if (nextDigits.length === 0) {
+      combinations.push(combination);
+      return;
+    }
+
+    const currentDigit = nextDigits[0];
+    const letters = digitToLetters[currentDigit];
+
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      backtrack(combination + letter, nextDigits.slice(1));
+    }
+  }
 }
 
-module.exports = letterCombinations;
+// Test case
+ // Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
